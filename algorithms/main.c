26880d83261a30745b860fce25c2704f0168af55
Arian Arfaian <arian.arfaian@gmail.com>
2014-03-12 23:46:45 -0400

    Initial Commit

---
+++ algorithms/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  algorithms
+//
+//  Created by Arian Arfaian on 3/12/14.
+//  Copyright (c) 2014 arfaian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
